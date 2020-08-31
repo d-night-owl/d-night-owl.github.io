aca85c029e284ca01fa0d3031eff5cd3bd27bdc5
Deep <57810645+d-night-owl@users.noreply.github.com>
2020-08-31 22:51:08 +0530

    WEB

---
+++ JS/modal.js
@@ -0,0 +1,27 @@
+var modalButton = document.querySelector('.modal-btn');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
+
+modalButton.addEventListener('click', function(){
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
+
+
+
+
+
+var modalButton = document.querySelector('.skill-modal-programming');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
+
+modalButton.addEventListener('click', function(){
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
